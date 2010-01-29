3f3734e6c9545d033c8f4b634a9c05ca11811149
crumbjp <cat.s.panch@gmail.com>
2010-01-29 17:56:13 +0900

    0.1

--- src/main/java/jp/co/rakuten/util/collection/StdSet.java
+++ src/main/java/jp/co/rakuten/util/collection/StdSet.java
@@ -24,49 +24,77 @@ public interface StdSet<T> {
 	public long size();
 	
 	/**
+	 * <pre>
 	 * Associates the date with the identity itself.
+	 *   Will be failure. If already associated same identity data in this container.
+	 * </pre>
 	 * 
-	 * @param t specified data.
+	 * @param t specifying data.
 	 * @return true if succeed. false if failure .
 	 */
 	public boolean insert(T t);
+
 	/**
+	 * <pre>
+	 * Associates the date with the identity itself.
+	 *   Will be replace by new data. If already associated same identity data in this container.
+	 * </pre>
 	 * 
-	 * @param t
-	 * @return
+	 * @param t specifying data.
+	 * @return true if replacing.
 	 */
 	public boolean set(T t);
+
 	/**
-	 * 
-	 * @param t
-	 * @return
+	 * <pre>
+	 * Find data of having same identity in this container.
+	 * </pre>
+	 * @param t specifying data.
+	 * @return Returns iterator of pointing the data or end of container.
 	 */
 	public StdIterator<T> find(T t);
+
 	/**
+	 * <pre>
+	 * Find lowest data of upper than specifying in this container.
+	 * </pre>
 	 * 
-	 * @param t
-	 * @return
+	 * @param t specifying data.
+	 * @return Returns iterator of pointing the data or end of container.
 	 */
 	public StdIterator<T> upperBound(T t);
+
 	/**
+	 * <pre>
+	 * Find highest data of lower than specifying in this container.
+	 * </pre>
 	 * 
-	 * @param t
-	 * @return
+	 * @param t specifying data.
+	 * @return Returns iterator of pointing the data or end of container.
 	 */
 	public StdIterator<T> lowerBound(T t);
+
 	/**
+	 * <pre>
+	 * Find lowest data in this container.
+	 * </pre>
 	 * 
-	 * @return
+	 * @return Returns iterator of pointing the first data or end of container(when this container is empty).
 	 */
 	public StdIterator<T> begin();
+
 	/**
+	 * <pre>
+	 * Find highest data in this container.
+	 * </pre>
 	 * 
-	 * @return
+	 * @return Returns iterator of pointing the last data or end of container(when this container is empty).
 	 */
 	public StdIterator<T> last();
 	/**
-	 * 
-	 * @return
+	 * Iterator of pointing end of container.
+	 *    
+	 * @return Returns end of container.
 	 */
 	public StdIterator<T> end();
 }
